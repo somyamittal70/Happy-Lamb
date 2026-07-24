41b4645d0401361ba465431b1025dbf2e507acf2
Ankita Mishra <ankitamishra8763@gmail.com>
2026-07-24 12:58:04 +0530

    update

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+
+import TeamHero from "../components/team/Hero";
+
 import Teamstats from "../components/team/Teamstats";
 import Teamgrid from "../components/team/TeamGrid";
 import Teamcta from "../components/team/Teamcta";
@@ -7,20 +9,10 @@ export default function TeamPage() {
   return (
     <main>
      
-    
+     <TeamHero />
       <Teamstats />
       <Teamgrid />
       <Teamcta />
     </main>
   );
-=======
-import TeamHero from "../components/team/Hero";
-
-export default function TeamPage() {
-    return(
-        <>
-        <TeamHero />
-        </>
-    )
->>>>>>> a9e8ac81d4c4f00774d1d072c248eec885f0d5e9
 }
