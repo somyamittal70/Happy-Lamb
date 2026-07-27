585d1f1e4ca9580e290832404df8ac4aba3ff3d3
KinshukMittal <kinshuk.mittal@bizwebify.com>
2026-07-27 10:02:11 +0530

    first commit

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -2,7 +2,7 @@
 import TeamHero from "../components/team/Hero";
 
 import Teamstats from "../components/team/Teamstats";
-// import Teamgrid from "../components/team/TeamGrid";
+import Teamgrid from "../components/team/TeamGrid";
 import Teamcta from "../components/team/Teamcta";
 import TeamCards from "../components/team/TeamGrid";
 
